87c965521affef3bb1e8a61b43b5a89601b6bac4
nekovalue <nekovalue@gmail.com>
2021-10-27 15:35:34 +0300

    Init commit

---
+++ cmake-build-debug/CMakeFiles/Lab03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab03.dir/main.c.obj"
+  "Lab03.exe"
+  "Lab03.exe.manifest"
+  "Lab03.pdb"
+  "libLab03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
