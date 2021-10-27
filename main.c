5a592832e3ad4cb5f7a61219af40797e8ed59c6c
nekovalue <nekovalue@gmail.com>
2021-10-27 16:45:11 +0300

    array_init_neg()

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define BUFFER_SIZE 100
 
 int array_init()
 {
     int buffer[BUFFER_SIZE];
-    int max = 100, min = -150;
+    int max = 2000, min = -1000;
 
     for (int i = 0; i < sizeof(buffer)/sizeof(int); ++i)
     {
@@ -16,13 +17,39 @@ int array_init()
     for (int i = 0; i < sizeof(buffer)/sizeof(int); ++i)
     {
         if (i % 10 == 0) printf("\n");
-        printf("%d ", buffer[i]);
+        printf("%d\t", buffer[i]);
     }
+
+    return 0;
+}
+
+int array_init_neg()
+{
+    srand(time(NULL));
+
+    int buffer[BUFFER_SIZE];
+    int max = 2000, min = -1000;
+
+    int number_lim = -500, nl_count = 0;
+
+    for (int i = 0; i < sizeof(buffer)/sizeof(int); ++i)
+    {
+        buffer[i] = rand() % (max - min + 1) + min;
+    }
+
+    for (int i = 0; i < sizeof(buffer)/sizeof(int); ++i)
+    {
+        if (buffer[i] < 0 && buffer[i] > number_lim)
+            ++nl_count;
+    }
+
+    printf("Total count of numbers (0 > n > -500): %d\n", nl_count);
 
     return 0;
 }
 
 int main()
 {
+
     return 0;
 }
