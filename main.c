2949682d40f4c0ef4459bbcaf0b64ecefe651acc
nekovalue <nekovalue@gmail.com>
2021-10-27 15:41:12 +0300

    Trying pointers

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    double f = 1.2, x;
+    double *pf;
+    pf = &f;
+    x = *pf;
+
+    printf("pf=%p; px=%p; f=%f; x=%f\n", pf, &x , f, x);
+
+    f = 1.5;
+
+    printf("pf=%p; px=%p; f=%f; x=%f", pf, &x , f, x);
+
     return 0;
 }
