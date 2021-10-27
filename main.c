be2b8a4c5191a3097aa31c0d30672b366dd4a0a9
nekovalue <nekovalue@gmail.com>
2021-10-27 19:49:34 +0300

    Lab03 finished

--- main.c
+++ main.c
@@ -232,8 +232,102 @@ int float_matrix()
     return 0;
 }
 
+int fsmile()
+{
+    int a[10][10] = {
+            {0, 0, 1, 1, 1, 1, 1, 1, 0, 0,},
+            {0, 1, 0, 0, 0, 0, 0, 0, 1, 0,},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+            {1, 0, 1, 1, 0, 0, 1, 1, 0, 1,},
+            {1, 0, 1, 1, 0, 0, 1, 1, 0, 1,},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+            {1, 0, 1, 0, 0, 0, 0, 1, 0, 1,},
+            {1, 0, 0, 1, 1, 1, 1, 0, 0, 1,},
+            {0, 1, 0, 0, 0, 0, 0, 0, 1, 0,},
+            {0, 0, 1, 1, 1, 1, 1, 1, 0, 0,},
+    };
+
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("\n");
+        for (int j = 0; j < 10; ++j)
+        {
+            if (a[i][j])
+                printf("%d  ", a[i][j]);
+            else
+                printf("   ");
+        }
+    }
+    printf("\n");
+    return 0;
+}
+
+int sam01()
+{
+    srand(time(NULL));
+
+    int a[20], size = 20, min = 0, max = 25;
+    long int sum = 0;
+
+    INIT_ARRAY(a, size, rand() % (max - min + 1) + min)
+    PRINT_ARRAY(a, size)
+
+    for (int i = 0; i < size; ++i)
+    {
+        if (i % 2 != 0) sum += pow(a[1], 2);
+    }
+
+    printf("\n\nSum: %ld\n", sum);
+
+    return 0;
+}
+
+int sam02()
+{
+    srand(time(NULL));
+
+    double a[5][5], size = 5, min = 0.0, max = 20.0;
+
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            a[i][j] = fRand(min, max);
+        }
+    }
+
+    int changed, tmp;
+    for (int i = 0; i < size; ++i)
+    {
+        do {
+            changed = 0;
+            for (int j = 0; j < (size - 1); ++j)
+            {
+                if (a[i][j] > a[i][j + 1])
+                {
+                    tmp = a[i][j];
+                    a[i][j] = a[i][j + 1];
+                    a[i][j + 1] = tmp;
+                    ++changed;
+                }
+            }
+        } while(changed);
+    }
+
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            printf("%.3f\t", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
 int main()
 {
-    float_matrix();
+    sam02();
     return 0;
 }
